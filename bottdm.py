87917a54e30788492b5305f53dda1641cc5883bd
axmed72-coder <axmedovamubina72@gmail.com>
2026-01-04 18:40:46 +0500

    Update bottdm.py

--- bottdm.py
+++ bottdm.py
@@ -1,19 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 import requests
 import os
 
 app = Flask(__name__)
 
-# Telegram токен и чат ID из переменных окружения
+@app.route("/")
+def health_check():
+    return "OK"
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-# Главная страница с формой
-@app.route("/")
-def index():
-    return render_template("index.html")  # index.html должен лежать в папке templates/
-
-# Endpoint для отправки заявки
 @app.route("/submit", methods=["POST"])
 def submit():
     data = request.json
@@ -24,19 +21,13 @@ def submit():
 📞 Контакт: {data.get('contact')}
 """
 
-    # Отправляем сообщение в Telegram
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     requests.post(url, json={
         "chat_id": CHAT_ID,
         "text": text
     })
 
-    
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
-
-   
